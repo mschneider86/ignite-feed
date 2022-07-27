e5ee3b9907f5a96c5971d1333cc0fded144ff062
Matheus Schneider <matheus.ssm@gmail.com>
2022-07-27 16:32:16 -0300

    add: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Header } from '../src/components/Header';
 
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+
 import './global.css';
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>posts</main>
+      </div>
     </>
   );
 }
