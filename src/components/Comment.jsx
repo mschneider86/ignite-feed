7216f66365e14b64554f452122d3f7c88c80b376
Matheus Schneider <matheus.ssm@gmail.com>
2022-08-01 17:58:17 -0300

    add: comment component structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/mschneider86.png' alt='' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.auhtorAndTime}>
+              <strong>Matheus Schneider</strong>
+              <time title='July 28th 16:13' dateTime='2022-07-28 16:13:30'>
+                Published 1 hour ago
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom cara, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
